ab7239eb3b3a362717acfb66ae0756c9d4988d21
Samantha Martorana <smartorana@logisolve.com>
2026-04-04 11:55:54 -0700

    remove all files

--- main.js
+++
@@ -1,7 +0,0 @@
-import { GameApplication } from './game/GameApplication.js';
-
-const application = new GameApplication();
-application.start().catch((err) => {
-  console.error(err);
-  document.body.innerHTML = `<pre style="color:#f8fafc;padding:1rem;font-family:system-ui,monospace;">Failed to start: ${String(err?.message ?? err)}</pre>`;
-});
